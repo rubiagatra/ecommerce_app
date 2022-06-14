4d537e2786cd228b6fe774b9531671af53373f8b
Doni Rubiagatra <doni@zero-one-group.com>
2022-06-14 19:48:09 +0700

    refactor: feature first scenario
    Signed-off-by: Doni Rubiagatra <doni@zero-one-group.com>

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   // * For more info on error handling, see:
   // * https://docs.flutter.dev/testing/errors
   await runZonedGuarded(() async {
-    // * Entry point of the app
+    WidgetsFlutterBinding.ensureInitialized();
+    // turn off the # in the URLs on the web
     GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+    // * Entry point of the app
     runApp(const MyApp());
 
     // * This code will present some error UI if any uncaught exception happens
